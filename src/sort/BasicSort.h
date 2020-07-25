483ea77e4715f114bc04141e2175ac2f04135bba
linshiqi <lin047@foxmail.com>
2020-07-25 10:15:58 +0800

    add selection sort

---
+++ src/sort/BasicSort.h
@@ -0,0 +1,54 @@
+#pragma once
+
+#include "SortBase.h"
+
+namespace code047 {
+	/// <summary>
+	/// InsertSort algorithm implementation
+	/// Time complexity:	O(N^2 / 2) in worst case(descending order)
+	///						O(N-1) in best case(already in ascending order)
+	/// Space complexity:	O(1)
+	/// Best usage case:	sorting array is almost in ascending order and short array
+	/// </summary>
+	/// <typeparam name="T"></typeparam>
+	template<typename T>
+	class InsertSort : public SortBase<T> {
+	public:
+		InsertSort() {}
+		~InsertSort() {};
+		void sort(std::vector<T>& a) override;
+	};
+	template<typename T>
+	void InsertSort<T>::sort(std::vector<T>& a) {
+		for (int i = 1; i < a.size(); i++) {
+			for (int j = i; j > 0 && less(a[j], a[j-1]); j--) {
+				std::swap(a[j-1], a[j]);
+			}
+		}
+	}
+
+	/// <summary>
+	/// Selection sort
+	/// </summary>
+	/// <typeparam name="T"></typeparam>
+	template<typename T>
+	class SelectSort : public SortBase<T> {
+	public:
+		SelectSort() {}
+		~SelectSort() {};
+		void sort(std::vector<T>& a) override;
+	};
+	template<typename T>
+	void SelectSort<T>::sort(std::vector<T>& a) {
+		for (size_t i = 0; i < a.size()-1; i++) {
+			int min = i;
+			for (size_t j = i+1; j < a.size();j++) {
+				if (less(a[j], a[min])) {
+					min = j;
+				}
+			}
+			std::swap(a[i], a[min]);
+		}
+	}
+
+}
--- src/test/SortTest.cpp
+++ src/test/SortTest.cpp
@@ -1,6 +1,6 @@
 #include "../../include/catch.hpp"
 #include "../sort/SortBase.h"
-#include "../sort/InsertSort.h"
+#include "../sort/BasicSort.h"
 #include <memory>
 
 using namespace code047;
@@ -23,14 +23,21 @@ TEST_CASE("SortBase test") {
 		REQUIRE(isSorted == false);
 	}
 }
-
+void f(std::shared_ptr<SortBase<int>> &sb_ptr, std::vector<int> &a) {
+	sb_ptr->sort(a);
+	bool isSorted = sb_ptr->isSorted(a);
+	REQUIRE(isSorted == true);
+}
 TEST_CASE("InsertSort test") {
-	SECTION("sort test") {
-		std::vector<int> a = { 1,5,6,3,8,9,12,32,15,23,22,11,66 };
+	
+	std::vector<int> a = { 1,5,6,3,8,9,12,32,15,23,22,11,66 };
+	SECTION("insertSort test") {
 		std::shared_ptr<SortBase<int>> sb_ptr(new InsertSort<int>()); //this line code would reporting error "cannot instaniate abstract class, why?"
-
-		sb_ptr->sort(a);
-		bool isSorted = sb_ptr->isSorted(a);
-		REQUIRE(isSorted == true);
+		f(sb_ptr, a);
+	}
+	SECTION("selectSort test") {
+		std::shared_ptr<SortBase<int>> sb_ptr(new SelectSort<int>()); //this line code would reporting error "cannot instaniate abstract class, why?"
+		f(sb_ptr, a);
 	}
 }
+
