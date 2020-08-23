60e1b5ae61b4a7238878f57904907d77df91cb9d
Shiqi Lin <1073998819@qq.com>
2020-08-23 21:54:58 +0800

    Finish kmp

--- src/string/Kmp.cpp
+++ src/string/Kmp.cpp
@@ -21,38 +21,38 @@ namespace code047 {
 			return;
 		}
 		const size_t M = pat.length();
-		for (auto v : this->_dfa) {
+		for (auto &v : this->_dfa) {
 			v.resize(M, 0);
 		}
-		this->_dfa[0][0] = 1;	// 初始化第一个状态
+		this->_dfa[pat[0]][0] = 1;	// Init first state 
 		for (size_t X = 0, j = 1; j < M; j++) {
 			for (int c = 0; c < this->_R; c++) {
-				this->_dfa[c][j] = this->_dfa[c][X];	// 不匹配复制X之前的状态
+				this->_dfa[c][j] = this->_dfa[c][X];	// Miss match copy X state
 			}
-			this->_dfa[pat[j]][j] = j+1;		// 匹配进入下一状态
-			X = this->_dfa[pat[j]][X];			// 更新X状态
+			this->_dfa[pat[j]][j] = j+1;		// Match, go to next state
+			X = this->_dfa[pat[j]][X];			// Update X state
 		}
 	}
 
 	size_t Kmp::search(std::string& txt, std::string& pat) {
-		// 检查输入
+		// Check input
 		if (txt.empty() || pat.empty()) {
-			return 0;	// 空串返回0
+			return 0;	// Empty string return 0
 		}
 		const size_t N = txt.length();
 		const size_t M = pat.length();
-		// 创建状态机
+		
 		build_dfa(pat);
 		
-		// 执行KMP查找
+		// KMP search
 		size_t i, j;
 		for (i = 0, j = 0; i < N && j < M; i++) {
 			j = _dfa[txt[i]][j];
 		}
-		if (j == M) {	// 找到
+		if (j == M) {	// Found
 			return i - M;
 		}
-		return N;		// 没找到
+		return N;		// Not Found
 	}
 
 }
--- src/test/SubStringSearchTest.cpp
+++ src/test/SubStringSearchTest.cpp
@@ -7,10 +7,9 @@ TEST_CASE("×Ó×Ö·û´®²éÕÒ") {
 	std::string txt = "abfsdjfakdfladf";
 	std::string pat = "fsdj";
 	const size_t index = 2;
-	SubStringBase* s_ptr = new Kmp(127);
-	//std::shared_ptr<SubStringBase> s_ptr(new Kmp(127));
+	std::shared_ptr<SubStringBase> s_ptr(new Kmp(127));
 	SECTION("KMP") {
-		//s_ptr = std::make_shared<Kmp>(127);
+		s_ptr = std::make_shared<Kmp>(127);
 		size_t res = s_ptr->search(txt, pat);
 		REQUIRE(res == index);
 	}
